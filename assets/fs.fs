a7c1f41abdb21fce144b7cc39c5973267da78403
yorung <yorung@gmail.com>
2014-04-21 00:38:28 +0900

    concentric sine curves

--- assets/fs.fs
+++ assets/fs.fs
@@ -1,10 +1,18 @@
 precision mediump float;
+varying vec2 position;
 varying vec2 texcoord;
 uniform sampler2D sampler1;
 uniform sampler2D sampler2;
 uniform float time;
 void main() {
-	vec2 coord = vec2(texcoord.x + sin(texcoord.x * 25.0 + time * 2.0) / 200.0, texcoord.y + sin(texcoord.y * 25.0 + time * 3.0) / 200.0);  
+//	vec2 coord = vec2(texcoord.x + sin(texcoord.x * 25.0 + time * 2.0) / 200.0, texcoord.y + sin(texcoord.y * 25.0 + time * 3.0) / 200.0);  
+
+	float dist1 = length(position + vec2(0.5, 0.5));
+	float dist2 = length(position - vec2(0.5, 0.5));
+	vec2 coord = vec2(texcoord.x, texcoord.y
+		+ sin(dist1 * 8.7 + time * 8.0) / 800.0
+		+ sin(dist2 * 10.0 + time * 12.5) / 800.0);
+
 	float alpha = sin(time * 3.14 / 5.0) / 2.0 + 0.5;
 	vec4 c1 = texture2D(sampler1, coord);
 	vec4 c2 = texture2D(sampler2, coord);
